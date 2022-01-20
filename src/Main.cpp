07e5f1ddeec60e97de7a36f7a54b255e9f4ca99b
Magnus Ahlstromer V <mangusbeef133@gmail.com>
2022-01-20 13:26:02 -0700

    Capped the frame rate

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,29 @@
 #include "Game.h"
+#include <iostream>
 
 Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    uint32_t frameStart;
+    int frameTime;
+
     game = new Game();
     game->Init("Pong!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
     while (game->IsRunning())
     {
+        frameStart = SDL_GetTicks();
+
         game->HandleEvents();
         game->Update();
         game->Render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime) SDL_Delay(frameDelay - frameTime);
     }
 
     game->Clean();
